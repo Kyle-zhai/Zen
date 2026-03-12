7fc74b930754e0a6d04d778f090ea0a8616e4944
Kyle-zhai <yn.zhai0205@gmail.com>
2026-03-12 08:53:37 -0400

    Initial Commit

---
+++ ZenChoice/ZenChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenChoiceApp.swift
+//  ZenChoice
+//
+//  Created by Kyle on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ZenChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
